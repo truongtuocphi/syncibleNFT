6c214340ffde55e56459debe37d3e75838ab885e
Truong Tuoc Phi <truongphi754@gmail.com>
2024-07-29 21:40:49 +0700

    feat: update

--- src/components/pages/Experience/CreateNFT.tsx
+++ src/components/pages/Experience/CreateNFT.tsx
@@ -5,7 +5,13 @@ import React, { useState } from "react";
 const CreateNFT = ({ onNext }: { onNext: (data: any) => void }) => {
   const [fullName, setFullName] = useState("");
   const [csvFile, setCsvFile] = useState<File | null>(null);
+  const [issuedDate, setIssuedDate] = useState("");
   const [quantity, setQuantity] = useState(1);
+  const [contractAddress, setContractAddress] = useState("");
+  const [blockchain, setBlockchain] = useState<"Polygon" | "Ethereum">(
+    "Polygon"
+  );
+  const [role, setRole] = useState<"Teacher" | "Student">("Student");
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
   const handleCsvChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,13 +25,36 @@ const CreateNFT = ({ onNext }: { onNext: (data: any) => void }) => {
     if (!fullName && !csvFile) {
       newErrors.fullName = "Full Name or CSV file is required.";
     }
+    if (!issuedDate) {
+      newErrors.issuedDate = "Issued date is required.";
+    }
+    if (!quantity || quantity <= 0) {
+      newErrors.quantity = "Quantity must be greater than 0.";
+    }
+    if (!contractAddress) {
+      newErrors.contractAddress = "Contract address is required.";
+    }
+    if (!blockchain) {
+      newErrors.blockchain = "Blockchain selection is required.";
+    }
+    if (!role) {
+      newErrors.role = "Role selection is required.";
+    }
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
   };
 
   const handleSubmit = () => {
     if (validate()) {
-      const data = { fullName, csvFile, quantity };
+      const data = {
+        fullName,
+        csvFile,
+        issuedDate,
+        quantity,
+        contractAddress,
+        blockchain,
+        role,
+      };
       onNext(data);
     }
   };
@@ -63,7 +92,21 @@ const CreateNFT = ({ onNext }: { onNext: (data: any) => void }) => {
       </div>
       <div>
         <label className="block text-sm font-medium text-gray-700">
-          Quantity:
+          Issued Date:
+          <input
+            type="date"
+            value={issuedDate}
+            onChange={(e) => setIssuedDate(e.target.value)}
+            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
+          />
+        </label>
+        {errors.issuedDate && (
+          <p className="text-red-500 text-sm">{errors.issuedDate}</p>
+        )}
+      </div>
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          NFT Quantity:
           <input
             type="number"
             value={quantity}
@@ -71,12 +114,87 @@ const CreateNFT = ({ onNext }: { onNext: (data: any) => void }) => {
             className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
           />
         </label>
+        {errors.quantity && (
+          <p className="text-red-500 text-sm">{errors.quantity}</p>
+        )}
+      </div>
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          Wallet address:
+          <input
+            type="text"
+            value={contractAddress}
+            onChange={(e) => setContractAddress(e.target.value)}
+            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
+          />
+        </label>
+        {errors.contractAddress && (
+          <p className="text-red-500 text-sm">{errors.contractAddress}</p>
+        )}
+      </div>
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          Blockchain:
+        </label>
+        <div className="flex space-x-4 mt-2">
+          <button
+            type="button"
+            onClick={() => setBlockchain("Polygon")}
+            className={`px-4 py-2 rounded-md ${
+              blockchain === "Polygon"
+                ? "bg-blue-600 text-white"
+                : "bg-gray-200"
+            }`}
+          >
+            Polygon
+          </button>
+          <button
+            type="button"
+            onClick={() => setBlockchain("Ethereum")}
+            className={`px-4 py-2 rounded-md ${
+              blockchain === "Ethereum"
+                ? "bg-blue-600 text-white"
+                : "bg-gray-200"
+            }`}
+          >
+            Ethereum
+          </button>
+        </div>
+        {errors.blockchain && (
+          <p className="text-red-500 text-sm">{errors.blockchain}</p>
+        )}
+      </div>
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          Certificate specifically for:
+        </label>
+        <div className="flex space-x-4 mt-2">
+          <button
+            type="button"
+            onClick={() => setRole("Teacher")}
+            className={`px-4 py-2 rounded-md ${
+              role === "Teacher" ? "bg-blue-600 text-white" : "bg-gray-200"
+            }`}
+          >
+            Teacher
+          </button>
+          <button
+            type="button"
+            onClick={() => setRole("Student")}
+            className={`px-4 py-2 rounded-md ${
+              role === "Student" ? "bg-blue-600 text-white" : "bg-gray-200"
+            }`}
+          >
+            Student
+          </button>
+        </div>
+        {errors.role && <p className="text-red-500 text-sm">{errors.role}</p>}
       </div>
       <button
         onClick={handleSubmit}
         className="mt-4 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       >
-        Next
+        Mint
       </button>
     </div>
   );
--- src/components/pages/Experience/DefineTemplate.tsx
+++ src/components/pages/Experience/DefineTemplate.tsx
@@ -1,4 +1,3 @@
-// components/DefineTemplate.tsx
 "use client";
 
 import React, { useState } from "react";
@@ -15,9 +14,15 @@ const DefineTemplate = ({ onNext }: { onNext: (data: any) => void }) => {
   const [selectedTemplate, setSelectedTemplate] = useState<number | null>(null);
   const [nftName, setNftName] = useState("");
   const [description, setDescription] = useState("");
-  const [signature, setSignature] = useState("");
+  const [signatureImage, setSignatureImage] = useState<File | null>(null);
   const [fullName, setFullName] = useState("");
   const [certificateNumber, setCertificateNumber] = useState("");
+  const [textFont, setTextFont] = useState<number>(20); // Default font size set to 20
+  const [fontChuc, setFontChuc] = useState("");
+  const [authorizingOrgName, setAuthorizingOrgName] = useState("");
+  const [headOrgName, setHeadOrgName] = useState("");
+  const [headOrgPosition, setHeadOrgPosition] = useState("");
+  const [headOrgSignature, setHeadOrgSignature] = useState("");
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
   const handleTemplateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -32,16 +37,39 @@ const DefineTemplate = ({ onNext }: { onNext: (data: any) => void }) => {
     setTemplate(null); // Clear custom template if predefined template is selected
   };
 
+  const handleSignatureImageChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (e.target.files) {
+      setSignatureImage(e.target.files[0]);
+    }
+  };
+
   const validate = () => {
     const newErrors: { [key: string]: string } = {};
     if (!selectedTemplate && !template)
       newErrors.template = "Please select or upload a template.";
     if (!nftName) newErrors.nftName = "NFT name is required.";
     if (!description) newErrors.description = "Description is required.";
-    if (template && !signature) newErrors.signature = "Signature is required.";
+    if (template && !signatureImage)
+      newErrors.signatureImage = "Signature image is required.";
     if (template && !fullName) newErrors.fullName = "Full name is required.";
     if (template && !certificateNumber)
       newErrors.certificateNumber = "Certificate number is required.";
+    if (template && !textFont) newErrors.textFont = "Text Font is required.";
+    if (template && !fontChuc) newErrors.fontChuc = "Font chữ is required.";
+    if (template && !authorizingOrgName)
+      newErrors.authorizingOrgName =
+        "Authorizing Organization name is required.";
+    if (template && !headOrgName)
+      newErrors.headOrgName = "Head of Organization name is required.";
+    if (template && !headOrgPosition)
+      newErrors.headOrgPosition =
+        "Head of Organization’s position is required.";
+    if (template && !headOrgSignature)
+      newErrors.headOrgSignature =
+        "Head of Organization signature is required.";
+
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
   };
@@ -53,9 +81,15 @@ const DefineTemplate = ({ onNext }: { onNext: (data: any) => void }) => {
         selectedTemplate,
         nftName,
         description,
-        signature,
+        signatureImage,
         fullName,
         certificateNumber,
+        textFont,
+        fontChuc,
+        authorizingOrgName,
+        headOrgName,
+        headOrgPosition,
+        headOrgSignature,
       };
       onNext(data);
     }
@@ -133,55 +167,94 @@ const DefineTemplate = ({ onNext }: { onNext: (data: any) => void }) => {
           <p className="text-red-500 text-sm">{errors.description}</p>
         )}
       </div>
-      {template && (
-        <>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Signature:
-              <input
-                type="text"
-                value={signature}
-                onChange={(e) => setSignature(e.target.value)}
-                className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
-              />
-            </label>
-            {errors.signature && (
-              <p className="text-red-500 text-sm">{errors.signature}</p>
-            )}
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Full Name:
-              <input
-                type="text"
-                value={fullName}
-                onChange={(e) => setFullName(e.target.value)}
-                className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
-              />
-            </label>
-            {errors.fullName && (
-              <p className="text-red-500 text-sm">{errors.fullName}</p>
-            )}
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Certificate Number:
-              <input
-                type="text"
-                value={certificateNumber}
-                onChange={(e) => setCertificateNumber(e.target.value)}
-                className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
-              />
-            </label>
-            {errors.certificateNumber && (
-              <p className="text-red-500 text-sm">{errors.certificateNumber}</p>
-            )}
-          </div>
-        </>
-      )}
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          Text Font:
+          <input
+            type="number"
+            value={textFont}
+            onChange={(e) => setTextFont(Number(e.target.value))}
+            defaultValue={20}
+            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
+          />
+        </label>
+        {errors.textFont && (
+          <p className="text-red-500 text-sm">{errors.textFont}</p>
+        )}
+      </div>
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          Font chữ:
+          <input
+            type="text"
+            value={fontChuc}
+            onChange={(e) => setFontChuc(e.target.value)}
+            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
+          />
+        </label>
+        {errors.fontChuc && (
+          <p className="text-red-500 text-sm">{errors.fontChuc}</p>
+        )}
+      </div>
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          Authorizing Organization Name:
+          <input
+            type="text"
+            value={authorizingOrgName}
+            onChange={(e) => setAuthorizingOrgName(e.target.value)}
+            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
+          />
+        </label>
+        {errors.authorizingOrgName && (
+          <p className="text-red-500 text-sm">{errors.authorizingOrgName}</p>
+        )}
+      </div>
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          Head of Organization Name:
+          <input
+            type="text"
+            value={headOrgName}
+            onChange={(e) => setHeadOrgName(e.target.value)}
+            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
+          />
+        </label>
+        {errors.headOrgName && (
+          <p className="text-red-500 text-sm">{errors.headOrgName}</p>
+        )}
+      </div>
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          Head of Organization’s Position:
+          <input
+            type="text"
+            value={headOrgPosition}
+            onChange={(e) => setHeadOrgPosition(e.target.value)}
+            className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
+          />
+        </label>
+        {errors.headOrgPosition && (
+          <p className="text-red-500 text-sm">{errors.headOrgPosition}</p>
+        )}
+      </div>
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          Signature Image:
+          <input
+            type="file"
+            accept="image/jpeg, image/png"
+            onChange={handleSignatureImageChange}
+            className="mt-1 block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer focus:outline-none p-2"
+          />
+        </label>
+        {errors.signatureImage && (
+          <p className="text-red-500 text-sm">{errors.signatureImage}</p>
+        )}
+      </div>
       <button
         onClick={handleSubmit}
-        className="mt-4 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        className="mt-4 w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       >
         Next
       </button>
